7a86be79d2eff6dedce4e1c20425e008d247f219
Felipe Cabral <38699509+fkbral@users.noreply.github.com>
2022-01-21 21:52:00 -0300

    style: atualização de estilização de código

--- exercicios/01_projeto_react/src/App.jsx
+++ exercicios/01_projeto_react/src/App.jsx
@@ -9,26 +9,22 @@ import { PersonsList } from './components/PersonsList'
 import { AttendanceList } from './components/AttendanceList'
 
 function App() {
-  const imageUrl = "https://images.unsplash.com/photo-1633356122102-3fe601e05bd2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80https://images.unsplash.com/photo-1633356122102-3fe601e05bd2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+  const imageUrl =
+    'https://images.unsplash.com/photo-1633356122102-3fe601e05bd2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80https://images.unsplash.com/photo-1633356122102-3fe601e05bd2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
 
-  const defaultImgUrl = "https://source.unsplash.com/random/?city,night"
+  const defaultImgUrl = 'https://source.unsplash.com/random/?city,night'
 
-  const images = [
-    defaultImgUrl,
-    defaultImgUrl,
-    defaultImgUrl,
-    defaultImgUrl,
-  ]
+  const images = [defaultImgUrl, defaultImgUrl, defaultImgUrl, defaultImgUrl]
 
   return (
-    <div className='app'>
+    <div className="app">
       {/* <img src={imageUrl} alt="foto da tela de um laptop" width={180} /> */}
       {/* <img src={imageUrl} alt="" style={{width: 180, objectFit: 'cover'}} /> */}
 
       {/* <Counter /> */}
       {/* <PersonsList /> */}
       <AttendanceList />
-{/* 
+      {/* 
       <Button
         isLoading={true}
         size={96}
